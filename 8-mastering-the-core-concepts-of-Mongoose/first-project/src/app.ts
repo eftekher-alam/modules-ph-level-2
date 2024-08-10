e0c75b1bd36ee20d35b84726b2f0db08a68c096e
eftekher-alam <eftekher@yahoo.com>
2024-08-10 12:46:36 +0600

    setup ts-node-dev to run typescrip like nodemon

--- 8-mastering-the-core-concepts-of-Mongoose/first-project/src/app.ts
+++ 8-mastering-the-core-concepts-of-Mongoose/first-project/src/app.ts
@@ -2,14 +2,10 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 
 const app: Application = express();
-const port = 3000;
 
 // parsers
 app.use(express.json());
 app.use(cors());
-
-const name = "shuvo";
-console.log(name);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
