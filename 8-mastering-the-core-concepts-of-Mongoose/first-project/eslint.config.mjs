2e36c4c030bcd1cb8d59381d85a90c2919398f6e
eftekher-alam <eftekher@yahoo.com>
2024-08-11 16:57:50 +0600

    Student schema is done

--- 8-mastering-the-core-concepts-of-Mongoose/first-project/eslint.config.mjs
+++ 8-mastering-the-core-concepts-of-Mongoose/first-project/eslint.config.mjs
@@ -4,13 +4,16 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  { ignores: ["dist/"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
-  {
-    rules: {},
-  },
+    { ignores: ["dist/"] },
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        rules: {
+            indent: ["error", 4],
+            "prettier/prettier": ["error", { tabWidth: 4 }],
+        },
+    },
 ];
--- 8-mastering-the-core-concepts-of-Mongoose/first-project/src/app/modules/student/student.interface.ts
+++ 8-mastering-the-core-concepts-of-Mongoose/first-project/src/app/modules/student/student.interface.ts
@@ -1,31 +1,43 @@
-import { Schema, model, connect } from "mongoose";
-
 // here we can user type that will consider as interface
 // or we can use interface directly
 
-export type TGuardian = {
-  fatherName: string;
-  fatherOccupation: string;
-  fatherContactNo: string;
-  motherName: string;
-  motherOccupation: string;
-  motherContactNo: string;
+type TGuardian = {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContactNo: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContactNo: string;
 };
 
-export type TStudent = {
-  id: string;
-  name: {
+type TName = {
     firstName: string;
     middleName: string;
     lastName: string;
-  };
-  email: string;
-  gender: "male" | "female";
-  dateOfBirth: string;
-  contactNo: string;
-  emergencyContactNo: string;
-  bloodGroup?: "O +" | "O -" | "A +" | "A +" | "B +" | "B -" | "AB +" | "AB -";
-  presentAddress: string;
-  permanentAddress: string;
-  guardian: TGuardian;
 };
+
+type TLocalGuardian = {
+    name: string;
+    occupation: string;
+    contact: string;
+    address: string;
+};
+
+type TStudent = {
+    id: string;
+    name: TName;
+    email: string;
+    gender: "male" | "female";
+    dateOfBirth?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    bloodGroup?: "O+" | "O-" | "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-";
+    presentAddress: string;
+    permanentAddress: string;
+    guardian: TGuardian;
+    localGuardian: TLocalGuardian;
+    profileImg?: string;
+    isActive: "active" | "inactive";
+};
+
+export { TGuardian, TName, TLocalGuardian, TStudent };
